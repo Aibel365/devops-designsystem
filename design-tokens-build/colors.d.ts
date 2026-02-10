1a7249a27b16adef21cfbc92b5533078491b52c5
Queen ✨ <mari.svennevik.notland@aibel.com>
2026-02-10 15:00:34 +0100

    Laget aibel-theme med themebuilder

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,24 @@
+/* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
+/* build: v1.11.1 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    'bright-blue': never;
+    'light-blue': never;
+    'aibel-blue': never;
+    'light-green': never;
+    green: never;
+    'light-pink': never;
+    'dark-pink': never;
+    'dark-red': never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
