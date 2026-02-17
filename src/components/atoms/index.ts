2273159b79ec8d5d9d0c5da976ec56665eb14a6e
Chetali Naigaonkar <chetali.naigaonkar@aibel.com>
2026-02-17 12:30:52 +0100

    add tag,tooltip and switch component with basic config

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -33,3 +33,12 @@ export type { PopoverProps } from "./popover/Popover";
 
 export type { IconSvgProps } from "./logo/AibelLogo";
 export { AibelLogo } from "./logo/AibelLogo";
+
+export { Tooltip } from "./tooltip/Tooltip";
+export type { TooltipProps } from "./tooltip/Tooltip";
+
+export { Tag } from "./tag/Tag";
+export type { TagProps } from "./tag/Tag";
+
+export { Switch } from "./switch/Switch";
+export type { SwitchProps } from "./switch/Switch";
