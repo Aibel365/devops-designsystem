aa09446ce86cbd11757562b61e4fb0c543b88bb5
William Amundsen Winterstø <william.amundsen.winterstoe@aibel.com>
2026-02-27 09:39:07 +0100

    Heading

---
+++ src/components/atoms/heading/Heading.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { Heading } from "./Heading";
+
+const meta: Meta<typeof Heading> = {
+    title: "Atoms/Heading",
+    component: Heading
+};
+
+type Story = StoryObj<typeof Heading>;
+
+export const Default: Story = {};
+
+export default meta;
