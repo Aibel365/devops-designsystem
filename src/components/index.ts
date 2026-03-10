cf3ed6cc9fbab07bdcc51e3bd3a4b6b9cee5b7cb
Ronny Valentin Vindenes <ronny.valentin.vindenes@aibel.com>
2026-03-10 15:20:23 +0100

    Remove unused depencies and code that now will be part of the aibel template

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./conditionRender/ConditionRender";
+export * from "./headerbar/HeaderBar";
+export * from "./headerbar/SubHeaderBar";
+export * from "./icons";
 export * from "./image/Base64ImageDisplay";
 export * from "./logo/AibelLogo";
-export * from "./headerbar/SubHeaderBar";
-export * from "./headerbar/HeaderBar";
-export * from "./icons";
-export { default as AibelBaseLayout } from "./layouts/AibelBaseLayout";
