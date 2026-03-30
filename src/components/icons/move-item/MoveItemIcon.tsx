f0b8949a996508de3d006cecbdc9ef3371c90c32
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-03-30 14:10:01 +0200

    Change how Icons are used to make it easier to adjust the styling

--- src/components/icons/move-item/MoveItemIcon.tsx
+++ src/components/icons/move-item/MoveItemIcon.tsx
@@ -1,17 +1,23 @@
 /// <reference types="vite-plugin-svgr/client" />
 import { SVGProps } from "react";
-import MoveItem from "./moveItem.jsx.svg?react";
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
     size?: number;
+    className?: string;
 };
 
-export const MoveItemIcon = ({ size, ...rest }: IconSvgProps) => {
-    const sizeCss = size ? `ads:size-[${size}px]` : "ads:size-4";
+export const MoveItemIcon = ({ size, className, ...rest }: IconSvgProps) => {
     return (
-        <MoveItem
-            className={`${sizeCss} ads:leading-0 ads:inline-block`}
+        <svg
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 -960 960 960"
+            height={size ?? 16}
+            width={size ?? 16}
+            className={`ads:leading-0 ads:inline-block ${className}`}
             {...rest}
-        />
+        >
+            <path d="M824-450H323v-60h501l-89-89 43-43 162 162-162 162-43-43 89-89ZM580-570v-210H180v600h400v-210h60v210q0 24.75-17.625 42.375T580-120H180q-24.75 0-42.375-17.625T120-180v-600q0-24.75 17.625-42.375T180-840h400q24.75 0 42.375 17.625T640-780v210h-60Z" />
+        </svg>
     );
 };
