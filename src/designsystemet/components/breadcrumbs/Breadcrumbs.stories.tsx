0ba79f777c8321fa63802306d2520617b8f83651
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-08 12:37:45 +0200

    Remove ol styling in Breadcrumbs and extend component stories

--- src/designsystemet/components/breadcrumbs/Breadcrumbs.stories.tsx
+++ src/designsystemet/components/breadcrumbs/Breadcrumbs.stories.tsx
@@ -4,36 +4,47 @@ import { Breadcrumbs } from "./Breadcrumbs";
 
 const meta: Meta<typeof Breadcrumbs> = {
     title: "Designsystemet/Breadcrumbs",
-    component: Breadcrumbs
+    component: Breadcrumbs,
+    args: { "data-size": "md" },
+    argTypes: {
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] },
+        "data-color": { control: { type: "radio" }, options: ["accent", "neutral"] },
+        children: {
+            control: false,
+            table: { disable: true }
+        }
+    }
 };
 
 type Story = StoryObj<typeof Breadcrumbs>;
 
 export const Default: Story = {
-    render: () => (
-        <Breadcrumbs aria-label="Du er her:">
-            <Breadcrumbs.Link
-                href="#"
-                aria-label="Tilbake til Nivå 3"
-            >
-                Nivå 3
-            </Breadcrumbs.Link>
-            <Breadcrumbs.List>
-                <Breadcrumbs.Item>
-                    <Breadcrumbs.Link href="#">Nivå 1</Breadcrumbs.Link>
-                </Breadcrumbs.Item>
-                <Breadcrumbs.Item>
-                    <Breadcrumbs.Link href="#">Nivå 2</Breadcrumbs.Link>
-                </Breadcrumbs.Item>
-                <Breadcrumbs.Item>
-                    <Breadcrumbs.Link href="#">Nivå 3</Breadcrumbs.Link>
-                </Breadcrumbs.Item>
-                <Breadcrumbs.Item>
-                    <Breadcrumbs.Link href="#">Nivå 4</Breadcrumbs.Link>
-                </Breadcrumbs.Item>
-            </Breadcrumbs.List>
-        </Breadcrumbs>
-    )
+    args: {
+        children: (
+            <>
+                <Breadcrumbs.Link
+                    href="#"
+                    aria-label="Tilbake til Nivå 3"
+                >
+                    Nivå 3
+                </Breadcrumbs.Link>
+                <Breadcrumbs.List>
+                    <Breadcrumbs.Item>
+                        <Breadcrumbs.Link href="#">Nivå 1</Breadcrumbs.Link>
+                    </Breadcrumbs.Item>
+                    <Breadcrumbs.Item>
+                        <Breadcrumbs.Link href="#">Nivå 2</Breadcrumbs.Link>
+                    </Breadcrumbs.Item>
+                    <Breadcrumbs.Item>
+                        <Breadcrumbs.Link href="#">Nivå 3</Breadcrumbs.Link>
+                    </Breadcrumbs.Item>
+                    <Breadcrumbs.Item>
+                        <Breadcrumbs.Link href="#">Nivå 4</Breadcrumbs.Link>
+                    </Breadcrumbs.Item>
+                </Breadcrumbs.List>
+            </>
+        )
+    }
 };
 
 export default meta;
