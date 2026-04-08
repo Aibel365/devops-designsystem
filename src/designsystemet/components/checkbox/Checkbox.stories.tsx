11abdcd8f07aa9b5706fda10794542130d985c54
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-08 16:49:51 +0200

    Extend Checkbox stories

--- src/designsystemet/components/checkbox/Checkbox.stories.tsx
+++ src/designsystemet/components/checkbox/Checkbox.stories.tsx
@@ -7,15 +7,38 @@ const meta: Meta<typeof Checkbox> = {
     component: Checkbox,
     args: {
         description: "Description"
+    },
+    argTypes: {
+        readOnly: { control: { type: "boolean" }, options: [true, false] },
+        disabled: { control: { type: "boolean" }, options: [true, false] },
+        required: { control: { type: "boolean" }, options: [true, false] },
+        checked: { control: { type: "boolean" }, options: [true, false] },
+        "data-indeterminate": { control: { type: "boolean" }, options: [true, false] },
+        error: { control: { type: "text" } },
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] }
     }
 };
 
 type Story = StoryObj<typeof Checkbox>;
 
-export const Default: Story = {};
+export const Default: Story = {
+    args: {
+        readOnly: false,
+        disabled: false,
+        required: false,
+        checked: false,
+        error: "",
+        "data-indeterminate": false,
+        "data-size": "md"
+    }
+};
 
-export const ReadOnly: Story = { args: { readOnly: true } };
+export const ReadOnly: Story = { args: { readOnly: true, checked: true } };
 
 export const Disabled: Story = { args: { disabled: true } };
+
+export const Error: Story = { args: { error: "This field is required" } };
+
+export const Indeterminate: Story = { args: { checked: true, "data-indeterminate": true } };
 
 export default meta;
