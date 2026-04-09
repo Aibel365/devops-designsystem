2e60f34650f229166ff4b299d0c99c508fd0c93b
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-09 07:12:37 +0200

    Show the Divider between paragraphs

--- src/designsystemet/components/divider/Divider.stories.tsx
+++ src/designsystemet/components/divider/Divider.stories.tsx
@@ -1,14 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Divider } from "./Divider";
+import { Paragraph } from "../paragraph/Paragraph";
 
 const meta: Meta<typeof Divider> = {
     title: "Designsystemet/Divider",
-    component: Divider,
-    argTypes: {},
-    args: {}
+    component: Divider
 };
 
 type Story = StoryObj<typeof Divider>;
 
-export const Default: Story = {};
+export const Default: Story = {
+    render: () => (
+        <>
+            <Paragraph>Above the Divider</Paragraph>
+            <Divider />
+            <Paragraph>Below the Divider</Paragraph>
+        </>
+    )
+};
 export default meta;
