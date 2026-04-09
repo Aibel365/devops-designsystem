e05c8ecd2c406cf749e59698e25ddd44b8cae376
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-09 13:49:41 +0200

    Extend Popover stories

--- src/designsystemet/components/popover/Popover.stories.tsx
+++ src/designsystemet/components/popover/Popover.stories.tsx
@@ -3,18 +3,46 @@ import { Popover } from "./Popover";
 
 const meta: Meta<typeof Popover> = {
     title: "Designsystemet/Popover",
-    component: Popover
+    component: Popover,
+    args: {
+        placement: "top",
+        variant: "default",
+        "data-color": "aibel-blue",
+        "data-size": "md"
+    },
+    argTypes: {
+        placement: { control: { type: "select" }, options: ["none", "top", "right", "bottom", "left", "top-start", "top-end", "right-start", "right-end", "bottom-start", "bottom-end", "left-start", "left-end"] },
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] },
+        open: { control: { type: "boolean" }, options: [true, false] },
+        variant: { control: { type: "radio" }, options: ["default", "tinted"] },
+        "data-color": { control: { type: "select" }, options: ["aibel-blue", "bright-blue", "green", "neutral", "info", "success", "warning", "danger"] },
+        autoPlacement: { control: { type: "boolean" }, options: [true, false] },
+        onOpen: {},
+        onClose: {}
+    }
 };
 
 type Story = StoryObj<typeof Popover>;
 
 export const Default: Story = {
-    render: () => (
+    render: (args) => (
         <Popover.TriggerContext>
-            <Popover.Trigger>Åpne popover</Popover.Trigger>
-            <Popover placement="top">Popoveret gir en rask beskjed. Her kan du vise brukeren informasjon som er relevant for konteksten.</Popover>
+            <Popover.Trigger>Open popover</Popover.Trigger>
+            <Popover {...args}>The popover provides a short message that can be easily communicated to the user.</Popover>
         </Popover.TriggerContext>
     )
 };
 
+export const TintedOnTheRight: Story = {
+    args: {
+        variant: "tinted",
+        placement: "right"
+    },
+    render: (args) => (
+        <Popover.TriggerContext>
+            <Popover.Trigger>Open popover</Popover.Trigger>
+            <Popover {...args}>The popover provides a short message that can be easily communicated to the user.</Popover>
+        </Popover.TriggerContext>
+    )
+};
 export default meta;
