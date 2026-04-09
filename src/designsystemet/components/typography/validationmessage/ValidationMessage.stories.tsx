7137e0870b80c0649676774b329d505bce38f4aa
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-09 10:42:42 +0200

    Adjust ValidationMessage stories

--- src/designsystemet/components/typography/validationmessage/ValidationMessage.stories.tsx
+++ src/designsystemet/components/typography/validationmessage/ValidationMessage.stories.tsx
@@ -4,21 +4,29 @@ import { ValidationMessage } from "./ValidationMessage";
 
 const meta: Meta<typeof ValidationMessage> = {
     title: "Designsystemet/Typography/ValidationMessage",
-    component: ValidationMessage
+    component: ValidationMessage,
+    args: {
+        children: "This is a message",
+        "data-color": "info",
+        "data-size": "md"
+    },
+    argTypes: {
+        "data-color": { control: { type: "radio" }, options: ["info", "success", "warning", "danger"] },
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] }
+    }
 };
 
 type Story = StoryObj<typeof ValidationMessage>;
 
-export const Default: Story = {
+export const Default: Story = {};
+
+export const AllTypes: Story = {
     render: () => (
         <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-            <ValidationMessage data-color="danger">This is an error message.</ValidationMessage>
-
-            <ValidationMessage data-color="success">This is a success message.</ValidationMessage>
-
             <ValidationMessage data-color="info">This is an informational message.</ValidationMessage>
-
+            <ValidationMessage data-color="success">This is a success message.</ValidationMessage>
             <ValidationMessage data-color="warning">This is a warning message.</ValidationMessage>
+            <ValidationMessage data-color="danger">This is an error message.</ValidationMessage>
         </div>
     )
 };
