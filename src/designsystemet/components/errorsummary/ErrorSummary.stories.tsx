4a104759e9b0af707345222d1647875080762178
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-09 09:10:28 +0200

    Extend ErrorSummary story

--- src/designsystemet/components/errorsummary/ErrorSummary.stories.tsx
+++ src/designsystemet/components/errorsummary/ErrorSummary.stories.tsx
@@ -4,29 +4,37 @@ import { ErrorSummary } from "./ErrorSummary";
 
 const meta: Meta<typeof ErrorSummary> = {
     title: "Designsystemet/ErrorSummary",
-    component: ErrorSummary
+    component: ErrorSummary,
+    args: {
+        children: (
+            <ErrorSummary>
+                <ErrorSummary.Heading>To proceed, you must correct the following errors:</ErrorSummary.Heading>
+                <ErrorSummary.List>
+                    <ErrorSummary.Item>
+                        <ErrorSummary.Link href="#">Birthdate cannot be after the year 2005</ErrorSummary.Link>
+                    </ErrorSummary.Item>
+                    <ErrorSummary.Item>
+                        <ErrorSummary.Link href="#">Phone number can only contain digits</ErrorSummary.Link>
+                    </ErrorSummary.Item>
+                    <ErrorSummary.Item>
+                        <ErrorSummary.Link href="#">Email must be valid</ErrorSummary.Link>
+                    </ErrorSummary.Item>
+                </ErrorSummary.List>
+            </ErrorSummary>
+        ),
+        "data-size": "md"
+    },
+    argTypes: {
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] },
+        children: {
+            control: false,
+            table: { disable: true }
+        }
+    }
 };
 
 type Story = StoryObj<typeof ErrorSummary>;
 
-//TODO Something wrong with the styling i think, ErrorSummaryItems does not get dots in front of them like in the examples: https://designsystemet.no/en/components/docs/error-summary/code
-export const Default: Story = {
-    render: () => (
-        <ErrorSummary>
-            <ErrorSummary.Heading>To proceed, you must correct the following errors:</ErrorSummary.Heading>
-            <ErrorSummary.List>
-                <ErrorSummary.Item>
-                    <ErrorSummary.Link href="#">Birthdate cannot be after the year 2005</ErrorSummary.Link>
-                </ErrorSummary.Item>
-                <ErrorSummary.Item>
-                    <ErrorSummary.Link href="#">Phone number can only contain digits</ErrorSummary.Link>
-                </ErrorSummary.Item>
-                <ErrorSummary.Item>
-                    <ErrorSummary.Link href="#">Email must be valid</ErrorSummary.Link>
-                </ErrorSummary.Item>
-            </ErrorSummary.List>
-        </ErrorSummary>
-    )
-};
+export const Default: Story = {};
 
 export default meta;
