d35dfb074f9a24477d6b277c49b4d61c0420a628
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-09 10:36:15 +0200

    Adjust Label story

--- src/designsystemet/components/typography/label/Label.stories.tsx
+++ src/designsystemet/components/typography/label/Label.stories.tsx
@@ -4,11 +4,21 @@ import { Label } from "./Label";
 
 const meta: Meta<typeof Label> = {
     title: "Designsystemet/Typography/Label",
-    component: Label
+    component: Label,
+    args: {
+        children: "This is a label",
+        weight: "regular",
+        asChild: false
+    },
+    argTypes: {
+        weight: { control: { type: "select" }, options: ["regular", "medium", "semibold"] },
+        asChild: { control: { type: "boolean" }, options: [true, false] },
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] }
+    }
 };
 
 type Story = StoryObj<typeof Label>;
 
-export const Default: Story = { render: () => <Label>This is a label</Label> };
+export const Default: Story = {};
 
 export default meta;
