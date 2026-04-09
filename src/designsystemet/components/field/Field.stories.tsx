78c34379df8f50f37504a08b44316aabed702006
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-09 09:20:55 +0200

    Extend Field stories

--- src/designsystemet/components/field/Field.stories.tsx
+++ src/designsystemet/components/field/Field.stories.tsx
@@ -7,20 +7,62 @@ import { ValidationMessage } from "../validationmessage/ValidationMessage";
 
 const meta: Meta<typeof Field> = {
     title: "Designsystemet/Field",
-    component: Field
+    component: Field,
+    args: {
+        children: (
+            <Field>
+                <Label>Last name</Label>
+                <Field.Description>Last name cannot contain spaces</Field.Description>
+                <Input defaultValue="Smith Washington" />
+                <ValidationMessage>You cannot have spaces in your last name</ValidationMessage>
+            </Field>
+        ),
+        position: "start",
+        "data-size": "md",
+        "data-color": "accent"
+    },
+    argTypes: {
+        "data-color": { control: { type: "radio" }, options: ["accent", "neutral"] },
+        position: { control: { type: "radio" }, description: `Position of toggle inputs (radio, checkbox, switch) in field "start" "end"`, options: ["start", "end"] },
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] },
+        children: {
+            control: false,
+            table: { disable: true }
+        }
+    }
 };
 
 type Story = StoryObj<typeof Field>;
 
-export const Default: Story = {
-    render: () => (
-        <Field>
-            <Label>Last name</Label>
-            <Field.Description>Last name cannot contain spaces</Field.Description>
-            <Input defaultValue="Smith Washington" />
-            <ValidationMessage>You cannot have spaces in your last name</ValidationMessage>
-        </Field>
-    )
+export const Default: Story = {};
+
+export const Affix: Story = {
+    args: {
+        children: (
+            <>
+                <Label>How much does it cost?</Label>
+                <Field.Affixes>
+                    <Field.Affix>NOK</Field.Affix>
+                    <Input />
+                    <Field.Affix>a month</Field.Affix>
+                </Field.Affixes>
+            </>
+        )
+    }
+};
+
+export const Position: Story = {
+    args: {
+        children: (
+            <>
+                <Label>Turn on</Label>
+                <Input
+                    type="checkbox"
+                    role="switch"
+                />
+            </>
+        )
+    }
 };
 
 export default meta;
