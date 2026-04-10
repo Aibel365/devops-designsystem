b13aaddac01d4d83fe5e719d6886854ac2a3b6de
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-10 13:06:33 +0200

    Extend Select stories

--- src/designsystemet/components/select/Select.stories.tsx
+++ src/designsystemet/components/select/Select.stories.tsx
@@ -3,19 +3,19 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Select } from "./Select";
 import { Field } from "../field/Field";
 import { Label } from "../typography/label/Label";
+import { ValidationMessage } from "../typography/validationmessage/ValidationMessage";
+
+Select.displayName = "Select";
+Field.displayName = "Field";
+Label.displayName = "Label";
+ValidationMessage.displayName = "ValidationMessage";
 
 const meta: Meta<typeof Select> = {
     title: "Designsystemet/Select",
-    component: Select
-};
-
-type Story = StoryObj<typeof Select>;
-
-export const Default: Story = {
-    render: () => (
-        <Field>
-            <Label>Select a mountain</Label>
-            <Select defaultValue="">
+    component: Select,
+    args: {
+        children: (
+            <>
                 <Select.Option
                     value=""
                     disabled
@@ -30,9 +30,111 @@ export const Default: Story = {
                 <Select.Option value="vinson">Mount Vinson</Select.Option>
                 <Select.Option value="puncakjaya">Puncak Jaya</Select.Option>
                 <Select.Option value="kosciuszko">Mount Kosciuszko</Select.Option>
-            </Select>
-        </Field>
-    )
+            </>
+        ),
+        disabled: false,
+        readOnly: false,
+        "data-size": "lg",
+        defaultValue: "",
+        width: "full"
+    },
+    argTypes: {
+        children: { control: false, table: { disable: true } },
+        disabled: { control: "boolean" },
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] },
+        width: { control: { type: "radio" }, options: ["auto", "full"] },
+        readOnly: { control: "boolean" }
+    }
+};
+
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {
+    render: (args) => {
+        const { children, ...restArgs } = args;
+        return (
+            <Field>
+                <Label>Select a mountain</Label>
+                <Select {...restArgs}>{children}</Select>
+            </Field>
+        );
+    }
+};
+
+export const Disabled: Story = {
+    args: {
+        disabled: true
+    },
+    render: (args) => {
+        const { children, ...restArgs } = args;
+        return (
+            <Field>
+                <Label>Select a mountain</Label>
+                <Select {...restArgs}>{children}</Select>
+            </Field>
+        );
+    }
+};
+
+export const WithError: Story = {
+    args: {
+        "aria-invalid": true
+    },
+    render: (args) => {
+        const { children, ...restArgs } = args;
+        return (
+            <Field>
+                <Label>Select a mountain</Label>
+                <Select {...restArgs}>{children}</Select>
+                <ValidationMessage>Required</ValidationMessage>
+            </Field>
+        );
+    }
+};
+
+export const WithOptGroup: Story = {
+    args: {
+        children: (
+            <>
+                <Select.Optgroup label="Group1">
+                    <Select.Option value="everest">Mount Everest</Select.Option>
+                    <Select.Option value="aconcagua">Aconcagua</Select.Option>
+                    <Select.Option value="denali">Denali</Select.Option>
+                    <Select.Option value="kilimanjaro">Kilimanjaro</Select.Option>
+                </Select.Optgroup>
+                <Select.Optgroup label="Group2">
+                    <Select.Option value="elbrus">Elbrus</Select.Option>
+                    <Select.Option value="vinson">Mount Vinson</Select.Option>
+                    <Select.Option value="puncakjaya">Puncak Jaya</Select.Option>
+                    <Select.Option value="kosciuszko">Mount Kosciuszko</Select.Option>
+                </Select.Optgroup>
+            </>
+        )
+    },
+    render: (args) => {
+        const { children, ...restArgs } = args;
+        return (
+            <Field>
+                <Label>Select a mountain</Label>
+                <Select {...restArgs}>{children}</Select>
+            </Field>
+        );
+    }
+};
+
+export const ReadOnly: Story = {
+    args: {
+        readOnly: true
+    },
+    render: (args) => {
+        const { children, ...restArgs } = args;
+        return (
+            <Field>
+                <Label>Select a mountain</Label>
+                <Select {...restArgs}>{children}</Select>
+            </Field>
+        );
+    }
 };
 
 export default meta;
