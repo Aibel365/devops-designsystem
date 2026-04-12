dfa8923d74f3af331a030a0b242b93fe08dae9f8
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-12 19:35:08 +0200

    Extend Tag stories

--- src/designsystemet/components/tag/Tag.stories.tsx
+++ src/designsystemet/components/tag/Tag.stories.tsx
@@ -1,16 +1,32 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Tag } from "./Tag";
 
+Tag.displayName = "Tag";
+
 const meta: Meta<typeof Tag> = {
     title: "Designsystemet/Tag",
-    component: Tag
+    component: Tag,
+    args: {
+        variant: "default",
+        "data-size": "md",
+        "data-color": "bright-blue",
+        children: "Tag Text"
+    },
+    argTypes: {
+        variant: { control: { type: "radio" }, options: ["default", "outline"] },
+        "data-size": { control: { type: "radio" }, options: ["sm", "md", "lg"] },
+        "data-color": { control: { type: "radio" }, options: ["aibel-blue", "bright-blue", "green", "neutral"] },
+        children: { control: { type: "text" } }
+    }
 };
 
 type Story = StoryObj<typeof Tag>;
 
-export const Default: Story = {
+export const Default: Story = {};
+
+export const Outline: Story = {
     args: {
-        children: "Feb 15th 2026"
+        variant: "outline"
     }
 };
 
