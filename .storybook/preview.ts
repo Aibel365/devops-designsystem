bccc6de5057465a5107e4eef857c9614f6f7f37e
Dave <110535103+DeividasSvaikauskas@users.noreply.github.com>
2026-04-13 15:04:21 +0200

    Add introduction page to Storybook (#32)
    * Add introduction page to Storybook
    
    * Format file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -16,6 +16,19 @@ const preview: Preview = {
             // 'error' - fail CI on a11y violations
             // 'off' - skip a11y checks entirely
             test: "todo"
+        },
+
+        options: {
+            storySort: {
+                order: [
+                    "Introduction",
+                    "Demo",
+                    "Designsystemet",
+                    "Icons",
+                    "Components",
+                    "*" // everything else
+                ]
+            }
         }
     }
 };
