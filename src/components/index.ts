6ca7f3d52f48399dda45e8d1a0eb451a5493294b
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-04-27 15:39:40 +0200

    chore: do not export base64, keep it internal

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export * from "./headerbar/HeaderBar";
 export * from "./footerbar/FooterBar";
 export * from "./headerbar/SubHeaderBar";
 export * from "./icons";
-export * from "./image/Base64ImageDisplay";
 export * from "./logo/AibelLogo";
