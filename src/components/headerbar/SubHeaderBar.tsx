d72e017a6249ce466ea27a65f3b3dc4db75e62c5
Dave <110535103+DeividasSvaikauskas@users.noreply.github.com>
2026-04-28 08:54:37 +0200

    Refactor/move and or remove components (#42)
    * refactor: remove ConditionRender
    
    * chore: do not export base64, keep it internal
    
    * chore: remove hooks since they should be in template
    
    * fix: forgot to remove hook export

--- src/components/headerbar/SubHeaderBar.tsx
+++ src/components/headerbar/SubHeaderBar.tsx
@@ -1,5 +1,4 @@
 import { Paragraph } from "../../designsystemet/components/typography/paragraph/Paragraph";
-import { ConditionRender } from "../conditionRender/ConditionRender";
 
 export type SubHeaderBarProps = {
     centerContent?: React.ReactNode | string;
@@ -12,18 +11,9 @@ export const SubHeaderBar = ({ centerContent, projectNumber, leftContent, rightC
     return (
         <header className="ads:flex ads:items-center ads:justify-between ads:w-full ads:h-14 px-8">
             <div className="ads:flex-1">{leftContent && leftContent}</div>
-            {centerContent && (
-                <ConditionRender
-                    condition={typeof centerContent === "string"}
-                    fallback={centerContent}
-                >
-                    <Paragraph data-size="xl">{centerContent}</Paragraph>
-                </ConditionRender>
-            )}
+            {!!centerContent && (typeof centerContent === "string" ? <Paragraph data-size="xl">{centerContent}</Paragraph> : centerContent)}
             <div className="ads:flex ads:flex-1 ads:justify-end">{projectNumber && <div className="ads:flex ads:items-center ads:w-fit ads:h-7 ads:sm:h-6 ads:py-0 ads:px-6 ads:rounded-full ads:bg-bright-blue-background-tinted">{projectNumber}</div>}</div>
-            <ConditionRender condition={!!rightContent}>
-                <div className="ads:flex-1 ads:justify-end">{rightContent}</div>
-            </ConditionRender>
+            {!!rightContent && <div className="ads:flex-1 ads:justify-end">{rightContent}</div>}
         </header>
     );
 };
