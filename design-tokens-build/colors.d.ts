671d4e26ec9bf8d6a26aeba5a7216ad61360922a
DeividasSvaikauskas <deividas.svaikauskas@aibel.com>
2026-05-27 15:15:28 +0200

    feat: remove unused support colors and rename the others

--- design-tokens-build/colors.d.ts
+++ design-tokens-build/colors.d.ts
@@ -1,24 +1,20 @@
 /* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
 /* build: v1.14.0 */
-import type {} from '@digdir/designsystemet-types';
+import type {} from "@digdir/designsystemet-types";
 
 // Augment types based on theme
-declare module '@digdir/designsystemet-types' {
-  export interface ColorDefinitions {
-    'bright-blue': never;
-    'light-blue': never;
-    'aibel-blue': never;
-    'light-green': never;
-    green: never;
-    'light-pink': never;
-    'dark-pink': never;
-    'dark-red': never;
-    neutral: never;
-  }
-  export interface SeverityColorDefinitions {
-    info: never;
-    success: never;
-    warning: never;
-    danger: never;
-  }
+declare module "@digdir/designsystemet-types" {
+    export interface ColorDefinitions {
+        "aibel-bright-blue": never;
+        "aibel-blue": never;
+        "aibel-green": never;
+        "aibel-neutral": never;
+        neutral: never;
+    }
+    export interface SeverityColorDefinitions {
+        info: never;
+        success: never;
+        warning: never;
+        danger: never;
+    }
 }
