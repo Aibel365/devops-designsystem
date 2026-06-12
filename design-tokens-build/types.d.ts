d1f9ec09d272b7d87b93511bbc3c7e809cb3a44c
Ronny Valentin Vindenes <ronny.valentin.vindenes@aibel.com>
2026-06-12 14:49:37 +0200

    chore: format

--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -1,19 +1,19 @@
 /* build: v1.15.0 */
-import type {} from '@digdir/designsystemet-types';
+import type {} from "@digdir/designsystemet-types";
 
 // Augment types based on theme
-declare module '@digdir/designsystemet-types' {
-  export interface ColorDefinitions {
-    'aibel-bright-blue': never;
-    'aibel-blue': never;
-    'aibel-green': never;
-    'aibel-neutral': never;
-    neutral: never;
-  }
-  export interface SeverityColorDefinitions {
-    info: never;
-    success: never;
-    warning: never;
-    danger: never;
-  }
+declare module "@digdir/designsystemet-types" {
+    export interface ColorDefinitions {
+        "aibel-bright-blue": never;
+        "aibel-blue": never;
+        "aibel-green": never;
+        "aibel-neutral": never;
+        neutral: never;
+    }
+    export interface SeverityColorDefinitions {
+        info: never;
+        success: never;
+        warning: never;
+        danger: never;
+    }
 }
